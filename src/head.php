9a6586047d20ecdb6b39c9f91b97ab9ff099d8c2
= <=>
2022-12-05 21:04:37 +0500

    first

---
+++ src/head.php
@@ -0,0 +1,19 @@
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <style>
+    * {
+    font-family: sans-serif;
+}
+.card {
+    border-radius: 0 ;
+}
+.btn {
+    border-radius: 0;
+}
+.form-control {
+    border-radius: 0;
+}
+    </style>
+    
